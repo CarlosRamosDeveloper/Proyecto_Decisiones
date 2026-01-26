9fa29a967873eddc2d125e9c0e746988580cc4db
Carlos Ramos <carlosramosdev@protonmail.com>
2026-01-26 16:28:45 +0100

    Initial commit

---
+++ src/test/kotlin/org/cr_d/decisiones/DecisionesApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.cr_d.decisiones
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DecisionesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
