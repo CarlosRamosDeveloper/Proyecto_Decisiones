5d29ee8a024cb566d15302969bafb14cc677ae8d
Carlos Ramos <carlosramosdev@protonmail.com>
2026-02-04 21:59:53 +0100

    updated NPC rest controller

--- src/main/kotlin/org/cr_d/decisiones/controller/NpcRestController.kt
+++ src/main/kotlin/org/cr_d/decisiones/controller/NpcRestController.kt
@@ -1,23 +1,14 @@
 package org.cr_d.decisiones.controller
-/*
-import org.cr_d.decisiones.dto.NpcRequest
+
 import org.cr_d.decisiones.dto.NpcResponse
 import org.cr_d.decisiones.mapper.toResponse
-import org.cr_d.decisiones.model.NonPlayableCharacter
 import org.cr_d.decisiones.service.NpcService
-import org.cr_d.decisiones.usecases.CreateNpcUseCase
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/npcs")
 class NpcRestController (
     private val npcService: NpcService,
-    private val create: CreateNpcUseCase
 ){
     @GetMapping("")
     fun getNpcList() : List<NpcResponse>{
@@ -32,13 +23,4 @@ class NpcRestController (
 
         return null
     }
-// TODO: Quitar del rest cuando lo haga en el nonrest
-    @PostMapping("")
-    fun createNpc(@RequestBody npc: NpcRequest) : NonPlayableCharacter{
-        val newNpc = create.execute(npc)
-
-        return npcService.save(newNpc)
-    }
 }
-
- */
