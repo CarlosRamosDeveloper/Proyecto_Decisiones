df92906b080222236c95ae4145639e4a499ed10b
Carlos Ramos <carlosramosdev@protonmail.com>
2026-02-05 10:01:24 +0100

    Users now can create characters

--- src/main/kotlin/org/cr_d/decisiones/controller/PlayerCharacterController.kt
+++ src/main/kotlin/org/cr_d/decisiones/controller/PlayerCharacterController.kt
@@ -1,26 +1,34 @@
 package org.cr_d.decisiones.controller
 
+import org.cr_d.decisiones.dto.CharacterRequest
 import org.cr_d.decisiones.mapper.toResponse
+import org.cr_d.decisiones.service.CharacterPresetService
 import org.cr_d.decisiones.service.PlayerCharacterService
-import org.cr_d.decisiones.usecases.GetUsersCountUseCase
+import org.cr_d.decisiones.service.UserService
+import org.cr_d.decisiones.usecases.CreateCharacterUseCase
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 @RequestMapping("/characters")
 class PlayerCharacterController (
     private val characterService: PlayerCharacterService,
-    private val getUsers: GetUsersCountUseCase
+    private val usersService: UserService,
+    private val presetService: CharacterPresetService,
+    private val createCharacter: CreateCharacterUseCase
+
 ){
     // TODO: No habilitar el botón de crear si no hay usuarios
     @GetMapping("")
     fun getCharacters(model: Model): String{
         model.addAttribute("characters", characterService.getAllCharacters().map { it.toResponse() })
         model.addAttribute("title", "Listado de Personajes")
-        model.addAttribute("users", getUsers.execute())
+        model.addAttribute("users", usersService.getAllUsers().count())
 
         return "character/list"
     }
@@ -32,5 +40,25 @@ class PlayerCharacterController (
         model.addAttribute("character", character.toResponse())
 
         return "character/detail"
+    }
+
+    @GetMapping("/new")
+    fun createCharacter(model: Model): String {
+        val emptyCharacter = CharacterRequest(null, 0, 0, "")
+        model.addAttribute("character", emptyCharacter)
+        model.addAttribute("users", usersService.getAllUsers())
+        model.addAttribute("presets", presetService.getAllPresets())
+        model.addAttribute("title", "Crear Personaje")
+
+        return "character/form"
+    }
+
+    @PostMapping("")
+    fun saveUser(@ModelAttribute character : CharacterRequest): String {
+        val newCharacter = createCharacter.execute(character)
+
+        characterService.save(newCharacter)
+
+        return "redirect:/characters"
     }
 }
