83c4a8d16f8456e563268210b0bb714e6a6bc526
Carlos Ramos <carlosramosdev@protonmail.com>
2026-02-07 13:47:22 +0100

    Added PlayerDecision repository and service

---
+++ src/main/kotlin/org/cr_d/decisiones/repository/PlayerDecisionRepository.kt
@@ -0,0 +1,8 @@
+package org.cr_d.decisiones.repository
+
+import org.cr_d.decisiones.model.PlayerDecision
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface PlayerDecisionRepository : JpaRepository<PlayerDecision, Long>
---
+++ src/main/kotlin/org/cr_d/decisiones/service/PlayerDecisionService.kt
@@ -0,0 +1,17 @@
+package org.cr_d.decisiones.service
+
+import org.cr_d.decisiones.model.PlayerDecision
+import org.cr_d.decisiones.repository.PlayerDecisionRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+
+@Service
+class PlayerDecisionService (
+    private val playerDecisionRepository: PlayerDecisionRepository
+){
+    fun findAll(): List<PlayerDecision> = playerDecisionRepository.findAll()
+    fun findById(playerDecisionId: Long): PlayerDecision? = playerDecisionRepository.findByIdOrNull(playerDecisionId)
+    fun save(playerDecision: PlayerDecision): PlayerDecision = playerDecisionRepository.save(playerDecision)
+    fun delete(playerDecisionId: Long) = playerDecisionRepository.deleteById(playerDecisionId)
+
+}
