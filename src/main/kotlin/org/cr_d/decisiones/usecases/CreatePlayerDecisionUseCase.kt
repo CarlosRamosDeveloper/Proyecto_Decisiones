5f1dc0b4a95ed4a2673456940d69d35f7e5b564c
Carlos Ramos <carlosramosdev@protonmail.com>
2026-02-07 14:28:49 +0100

    Added create and save PlayerDecisionusecases

---
+++ src/main/kotlin/org/cr_d/decisiones/usecases/CreatePlayerDecisionUseCase.kt
@@ -0,0 +1,25 @@
+package org.cr_d.decisiones.usecases
+
+import org.cr_d.decisiones.dto.PlayerDecisionRequest
+import org.cr_d.decisiones.model.PlayerDecision
+import org.cr_d.decisiones.service.DecisionOptionService
+import org.cr_d.decisiones.service.DecisionService
+import org.cr_d.decisiones.service.PlayerCharacterService
+
+class CreatePlayerDecisionUseCase (
+    private val characterService: PlayerCharacterService,
+    private val decisionService: DecisionService,
+    private val optionService: DecisionOptionService
+) {
+    fun execute(playerDecision: PlayerDecisionRequest): PlayerDecision {
+        val character = characterService.getCharacterById(playerDecision.characterId)
+        val decision = decisionService.findById(playerDecision.decisionId)
+        val option = optionService.findById(playerDecision.optionId)
+
+        return PlayerDecision(
+            playerCharacter = character!!,
+            decision = decision!!,
+            decisionOption = option!!,
+        )
+    }
+}
---
+++ src/main/kotlin/org/cr_d/decisiones/usecases/UpdatePlayerDecisionUseCase.kt
@@ -0,0 +1,31 @@
+package org.cr_d.decisiones.usecases
+
+import org.cr_d.decisiones.dto.PlayerDecisionRequest
+import org.cr_d.decisiones.model.PlayerDecision
+import org.cr_d.decisiones.service.DecisionOptionService
+import org.cr_d.decisiones.service.DecisionService
+import org.cr_d.decisiones.service.PlayerCharacterService
+import org.cr_d.decisiones.service.PlayerDecisionService
+import java.time.LocalDateTime
+
+class UpdatePlayerDecisionUseCase(
+    private val playerDecisionService: PlayerDecisionService,
+    private val characterService: PlayerCharacterService,
+    private val decisionService: DecisionService,
+    private val optionService: DecisionOptionService
+) {
+    fun execute(playerDecision: PlayerDecisionRequest, id: Long): PlayerDecision {
+        val exists = playerDecisionService.findById(id)
+        val character = characterService.getCharacterById(playerDecision.characterId)
+        val decision = decisionService.findById(playerDecision.decisionId)
+        val option = optionService.findById(playerDecision.optionId)
+
+        return PlayerDecision(
+            id = id,
+            playerCharacter = character!!,
+            decision = decision!!,
+            decisionOption = option!!,
+            createdAt = exists?.createdAt ?: LocalDateTime.now(),
+        )
+    }
+}
