672523dea1e0720b9ccf5b040101d7081641e9e8
Carlos Ramos <carlosramosdev@protonmail.com>
2026-02-08 14:52:24 +0100

    Implemented findById

--- src/main/kotlin/org/cr_d/decisiones/controller/DecisionRestController.kt
+++ src/main/kotlin/org/cr_d/decisiones/controller/DecisionRestController.kt
@@ -5,12 +5,13 @@ import org.cr_d.decisiones.mapper.toResponse
 import org.cr_d.decisiones.service.DecisionService
 import org.cr_d.decisiones.usecases.GetAllOptionsByDecisionUseCase
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/decisions")
-class DecisionsRestController (
+class DecisionRestController (
     private val decisionsService: DecisionService,
     private val getOptions: GetAllOptionsByDecisionUseCase
 ){
@@ -22,6 +23,14 @@ class DecisionsRestController (
         }
 
         return response
+    }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id : Long): DecisionResponse? {
+        val response = decisionsService.findById(id) ?: return null
+        val options = getOptions.execute(response)
+
+        return response.toResponse(options)
     }
 
 
